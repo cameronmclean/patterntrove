3a0df4804895c8fecfed25e9e3670105a4f6ce6c
cameronmclean <ca.mclean@auckland.ac.nz>
2014-06-10 16:07:11 +1200

    moved tiny_mce/ to tiny_mce_backup/ and added RFDaCE as tinymce/

---
+++ docs/inc/tiny_mce_backup/plugins/insertlatex/langs/en.js
@@ -0,0 +1,3 @@
+tinyMCE.addI18n('en.insertlatex',{
+	desc : 'Insert/Edit Equation (LaTeX)',
+});
---
+++ docs/inc/tiny_mce_backup/plugins/insertlatex/langs/en_dlg.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('en.insertlatex_dlg',{
+	title : 'Equation Editor (LaTeX)',
+	render : 'Render',
+});
